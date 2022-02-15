10d848261dab9cb72b1e40f10e415da2029afa9d
Richard Opiyo <romenyo@dewcis.com>
2022-02-15 09:41:07 +0300

    fix linter errors

--- modules/BookList.js
+++ modules/BookList.js
@@ -1,7 +1,5 @@
 // eslint-disable-next-line import/no-cycle
-
-import Book from './Book.js';
-import { nav1 } from './Book.js';
+import { Book,nav1 } from './Book.js';
 import { DateTime } from '../node_modules/luxon/build/es6/luxon.js';
 
 export default class BookList {
