fc2c7ff5edc196e810be4d20753d534ad061dcbf
Richard Opiyo <romenyo@dewcis.com>
2022-02-15 10:13:09 +0300

    git refresh

--- modules/navigation.js
+++ modules/navigation.js
@@ -16,9 +16,11 @@ export default class Navigator {
     if (typeof (window) !== 'undefined') {
       window.addEventListener('popstate', () => {
         this.links = Array.from(document.querySelectorAll('nav a '));
+
         this.links.filter((el) => el.href !== window.location.href).forEach((el) => {
           el.classList.remove('active');
         });
+        
         this.links.filter((el) => el.href === window.location.href)[0].classList.add('active');
         this.navigate(window.location.hash);
       });
